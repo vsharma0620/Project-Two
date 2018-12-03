19d0c6a02b7fbff054e3c13870570acc11003608
Dardin <logandalec@gmail.com>
2018-12-03 00:16:36 -0800

    fixing stuff

--- public/js/add.js
+++ public/js/add.js
@@ -6,28 +6,32 @@ var category;
 // function submitNewUser () {
 if (category !== null) {
 
-  $("#submit-btn").on("submit", function (event) {
+  $("#Login").on("submit", function (event) {
     event.preventDefault();
     console.log("button has been clicked");
 
+    var presBool = ($("#presenter").find(':checked').val() === "presenter");
+
     // Make a new user object
     var newUser = {
-      username: $("#inputUsername").val().trim(),
-      category: $("#category").val().trim(),
-      presenter: $("#presenter").val().trim(),
+      username: $("#username").val().trim(),
+      category: $(this).find(':selected').val(),
+      presenter: presBool,
     };
 
-    // Send an AJAX POST-request with jQuery
+    console.log(newUser);
+
+    Send an AJAX POST-request with jQuery
     $.post("/api/newUser", newUser)
       // On success, run the following code
       .then(function (data) {
         // Log the data we found
         console.log(data);
         localStorage.setItem('user', JSON.stringify(newUser));
-      });
+     });
 
     // Empty each input box by replacing the value with an empty string
-    $("#inputUsername").val("");
+    $("#usernamesername").val("");
     $("#category").val("");
     $("#presenter").val("");
   });
