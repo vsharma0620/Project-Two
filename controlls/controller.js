658b9b9c4808f9c62d7be1351ad70e966ebf3d1c
Vanita <vanitasharma_16@hotmail.com>
2018-12-03 21:17:43 -0800

    css text-speech changes and pseudo-code controller

--- controlls/controller.js
+++ controlls/controller.js
@@ -9,21 +9,27 @@ var controller = {
     run: function() {
         var numUsers = users.numUsers();
 
-        if (users <= 2) {
-            users.db.Hook('afterCreate', function(user, options) {
-                
-        }
         //hook = listener on(user created)
-        
-        else if (user > 2) {
 
-        
+        if (numUsers < 2) {
+            users.db.Hook('afterCreate', function(user, options) {
+            //if the numUsers are less than 2 then we want it to keep checking for more presenters on deck.
+            //if the numUsers are equal to 2 then we want to call the readyOnDeck to get a presenter and then return the presenter
+            if (numUsers === 2) {
+
+                    setTimeout(function() {
+                        readyOnDeck
+                    
+                }, 150000);
+                
+        });
+    }
         // performs every five minutes
         setInterval(function(){
            // set Presenter. Swap changes everyones status in the user table. I need set the presenter after everyones swap and then set that username to the current presenster.
         
         }, 3000000);
-    }
+    
 }
 };
 
--- public/js/speechRecognitionMain.js
+++ public/js/speechRecognitionMain.js
@@ -34,48 +34,44 @@ function clearText() {
 }
 
 // function to submit the data to the server.
-function submitToServer(event){
+function submitToServer(event) {
     //console.log(event)
     event.preventDefault();
     var speech = {
-        //text: finalTranscript
-        text: "Brian is a wiz"
+        text: finalTranscript
+        // text: "Brian is a wiz"
     };
-    $.post("api/SpeechRecognitionMain", speech, function(req,res) {
+    $.post("api/presenter/text", speech)
 
-    console.log("======================")
-    console.log("Submitted to Server!")
-    });
+        console.log("======================")
+        console.log("Submitted to Server!")
 };
+
 
 recognition.onresult = (event) => {
     let interimTranscript = "";
     for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-    let transcript = event.results[i][0].transcript;
+        let transcript = event.results[i][0].transcript;
 
-    if (event.results[i].isFinal) {
-        finalTranscript += transcript + "\n";
-        currentTranscript = transcript;
+        if (event.results[i].isFinal) {
+            finalTranscript += transcript + "\n";
+            currentTranscript = transcript;
 
-        //aim to push the currentTranscript on to the SQL Server
-        
-        //finalTranscript = transcript;
-        console.log("Current Transcript: "+ currentTranscript)
-        ;
-                
-        $(".wordsContainer").append(currentTranscript + "<br>" + "<br>")
-    } else {
-        interimTranscript += transcript; // interimTranscript is not final
+            //aim to push the currentTranscript on to the SQL Server
+
+            //finalTranscript = transcript;
+            console.log("Current Transcript: " + currentTranscript);
+
+            $(".wordsContainer").append(currentTranscript + "<br>" + "<br>")
+        } else {
+            interimTranscript += transcript; // interimTranscript is not final
+        }
     }
-    }        
 }
 
-$(document).ready(function(){
-    // $("#btnStartRec").click(startRecognition); // button to start speech recognition
-    // $("#btnEndRec").click(stopRecognition); // button to stop speech recognition
-    // $("#btnClearRec").click(clearText); //button to clear the display 
-    //$("#btnSubmitRec").click(alert("Hello")); // button to push the final transcript to the server
-     $("#btnSubmitRec").click(submitToServer); // button to push the final transcript to the server
+$(document).ready(function () {
+    $("#btnStartRec").click(startRecognition); // button to start speech recognition
+    $("#btnEndRec").click(stopRecognition); // button to stop speech recognition
+    $("#btnClearRec").click(clearText); //button to clear the display 
+    $("#btnSubmitRec").click(submitToServer); // button to push the final transcript to the server
 });
-
-    
