c58b2db4e0a7a428a4eca21daf872fe622fb721b
Vanita <vanitasharma_16@hotmail.com>
2018-12-03 20:02:49 -0800

    Starting Controller

--- controlls/controller.js
+++ controlls/controller.js
@@ -9,12 +9,25 @@ var controller = {
     run: function() {
         var numUsers = users.numUsers();
 
+        if (users <= 2) {
+            users.db.Hook('afterCreate', function(user, options) {
+                
+        }
+        //hook = listener on(user created)
+        
+        else if (user > 2) {
+
+        
         // performs every five minutes
         setInterval(function(){
-
+           // set Presenter. Swap changes everyones status in the user table. I need set the presenter after everyones swap and then set that username to the current presenster.
+        
         }, 3000000);
     }
+}
 };
+
+//use moment to find current time and add five minutes to it. This will be set as the end time.
 
 
 module.exports = controller;
