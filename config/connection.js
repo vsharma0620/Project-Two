07e9908100ef18b13d3c985b051b542ac1430b29
Mona <monarahmanimehr@gmail.com>
2018-12-03 19:28:12 -0800

    added

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,7 @@ determine the current presenter, onDeck and audience */
 var users = require("../js/user-sequelize");
 var moment = require("moment");
 // Creates mySQL connection using Sequelize
-<<<<<<< HEAD
 var sequelize = new Sequelize("open_mic_db", "root", "mona12271", {
-
-=======
-var sequelize = new Sequelize("open_mic_db", "root", "Bruno123.", {
->>>>>>> bb11137a588bac0258027df69bb87000ca7e5829
   host: "localhost",
   port: 3306,
   dialect: "mysql",
