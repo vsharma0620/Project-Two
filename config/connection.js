b9a324c321e38e0d64973d6804c97d806cc1c84f
Dardin <logandalec@gmail.com>
2018-12-08 12:02:22 -0800

    jaws2

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ determine the current presenter, onDeck and audience */
 var users = require("../js/user-sequelize");
 var moment = require("moment");
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("open_mic_db", "root", "Bruno123.", {
-  host: "localhost",
+var sequelize = new Sequelize("open_mic_db", "umt8teu02eoe29r6", "enz00h8ux96ixhe2", {
+  host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   dialect: "mysql",
   pool: {
